15e446b8041386642f2200265eb224bea8426855
Tuba Nafees <tuba.nafees4@gmail.com>
2025-03-12 05:42:43 +0500

    Project modifies

--- app.py
+++ app.py
@@ -432,7 +432,7 @@ st.markdown("""
     /* Media Queries for Mobile */
     @media screen and (max-width: 400px) {
         .title {
-            font-size: 28px; /* Increased size for mobile below 400px */
+            font-size: 30px; /* Increased size for mobile below 400px */
             margin: 0.5rem 0; /* Adjusted for mobile */
         }
         
